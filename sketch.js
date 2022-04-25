c9f4b3fba5f9e8199138ef2278f3707b1af3bb8c
LittleTealeaf <LittleTealeaf@users.noreply.github.com>
2022-04-25 18:50:31 +0000

    deploy: 99cc8db56f32b85e429f1dfe0908b52e890a8b04

--- sketch.js
+++ sketch.js
@@ -1,9 +1,35 @@
 /// <reference path="./node_modules/@types/p5/global.d.ts" />
 
+/*
+Worked alongside Priscilla Esteves on Exercise
+*/
+var spr;
+var ct;
+
+var count = 0;
+
 function setup() {
-    createCanvas(windowWidth - 20,windowHeight - 20);
+  createCanvas(600, 400);
+  ct = 0;
 }
 
 function draw() {
-    background(220);
+  background(200);
+  drawSprites();
+
+  if (ct == 60) {
+    spr = createSprite(width / 2, 0, 40, 40);
+    spr.shapeColor = color(random(255),random(255),random(255)) ;
+    spr.onMousePressed = spriteClicked;
+    spr.velocity.y = 2;
+    ct = 0;
+  }
+  ct++;
+
+  text("Sprites Clicked: " + count, 50,50);
+}
+
+function spriteClicked() {
+    count++;
+    console.log(count);
 }
